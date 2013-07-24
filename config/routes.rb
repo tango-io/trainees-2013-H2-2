59c63d42b3e0a481b2b9e63bee712dd26714ea48
Alejandra Stephanía Cernas Larios <alejandra.cernas@tangosource.com>
2013-07-23 20:54:00 -0500

    index_page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Trainees2013H22::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+   root 'kickstarter#index'
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
---
+++ test/helpers/kickstarter_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class KickstarterHelperTest < ActionView::TestCase
+end
