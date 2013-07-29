1088c542cd2e55d5ef9bade6813ff330d03bf603
Johan Arturo Alvarado Hernández <johan@tangosource.com>
2013-07-29 17:12:26 -0500

    App created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starter::Application.config.secret_key_base = '42f42ab6c09b7cf0b55d3640a8f403c77c3a296140f35ea2155dbc3f1370f8c4b4f6a3cb33ab5629e386e0a4ad55965a7f669a65db60b9bf26db31fbe1f9ef81'
