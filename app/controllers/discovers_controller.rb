2df9fd84bd151048382a68810f0c92c5e70dc3c6
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-08-08 17:34:18 -0500

    Show projects in subcategories

--- app/controllers/discovers_controller.rb
+++ app/controllers/discovers_controller.rb
@@ -2,7 +2,7 @@ class DiscoversController < ApplicationController
   def index
     @category = Category.first
     @categories = Category.all
-    @projects_category = @category.projects.all
+    @projects_category = @category.projects.first(3)
     @subcategories_of = @category.subcategories.all
   end
 end
