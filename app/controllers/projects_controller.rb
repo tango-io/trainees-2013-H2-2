936fb2e420886b1862a3859afaec668fbb49c2a9
Jesus Romero <jose.romero@tangosource.com>
2013-08-07 20:11:41 -0500

    added method edit to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,10 +30,32 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+    @categories = Category.all
+  end
+
+  def update
+     @projects = Project.find(params[:id]);
+   if  @projects.update_attributes(project_params2)
+     @projects = Project.find(params[:id]);
+     cad = @projects.video.split('=')
+     video2 = "//www.youtube.com/embed/" + cad[1]
+     @projects.update(:video => video2)
+     redirect_to index
+   else 
+     render 'edit'
+   end
+  end
+
 private
 
   def project_params
     params.require(:project).permit(:name,:description, :content, :picture, :video, :goal, :period, :category_id)
+  end
+  
+  def project_params2
+    params.require(:project).permit(:description, :content, :picture, :video, :category_id)
   end
 
 end
