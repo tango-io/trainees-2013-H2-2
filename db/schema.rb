4c198c5822ca8171f746fd94b2e262915edec012
Héctor Alejandro Meza Valdez <hmezav@gmail.com>
2013-08-12 07:05:04 -0500

    gem devise added and some changes in layout _header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20130815035941) do
+=======
+ActiveRecord::Schema.define(version: 20130812111958) do
+>>>>>>> gem devise added and some changes in layout _header
 
   create_table "backers", force: true do |t|
     t.string   "user_id"
