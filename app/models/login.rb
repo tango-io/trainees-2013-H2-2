4c89be77f63ea902d6740f303628f494e99fc9da
Johan Arturo Alvarado Hernández <johan@tangosource.com>
2013-08-14 14:59:03 -0500

    validate empty fields and match fields

--- app/models/login.rb
+++ app/models/login.rb
@@ -3,16 +3,26 @@ class Login < ActiveRecord::Base
   attr_accessor :repassword
 
   #only presence all
-  def self.presence?(inst,params)
-   # @login = Login.new
-    params.each do |key|
-      key.each do |k,v|
-        if v.to_s.length == 0
-          inst.errors.add(:fullname,'watffkfjf')
-          inst
-          #Login.errors.add(:fullname,'wtf')
-        end
+  def self.presence?(instance,params)    
+    @instance = instance
+    @params = params
+    @params.each{|key, value|
+      if value.length == 0
+        #puts "key = #{key} and v= #{value}"
+        @instance.errors.add(key.parameterize.underscore.to_sym,'Please enter all methods')           
       end
+    }
+    compareTo?
+   # binding.pry
+  end
+  def self.compareTo?
+    if @params['email'] != @params['reemail']
+      @instance.errors.add('email','Email not match with reemail')           
     end
+    if @params['password'] != @params['repassword']
+      @instance.errors.add('password','Password not match with reepasswors')
+    end
+    @instance
+
   end
 end
