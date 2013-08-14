78595e5e35755ef63f00f6052d7769d0af8269fa
Johan Arturo Alvarado Hernández <johan@tangosource.com>
2013-08-14 12:42:40 -0500

    Add form validation

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,18 +1,19 @@
 class LoginsController < ApplicationController
   def index
     @login = Login.new
-    render  :new
   end
   def new
     @login = Login.new
   end
   def create
-    puts "loginparams #{loginParams}"
-
-    binding.pry
+    @login = Login.new(loginParams)
+    if @login.save
+      render 'new'
+    else
+      render 'new'
+    end
   end
   def loginParams
-    params.require(:login).permit(:fullname,:email,:reemail,:repassword)
+    params.require(:login).permit(:fullname,:email,:password,:reemail,:repassword)
   end
 end
-
--- app/models/login.rb
+++ app/models/login.rb
@@ -1,4 +1,5 @@
 class Login < ActiveRecord::Base
-    attr_accessor :reemail
-    attr_accessor :repassword
+  attr_accessor :reemail
+  attr_accessor :repassword
+  validates :fullname, :email, :password, :repassword, :reemail, presence: true
 end
