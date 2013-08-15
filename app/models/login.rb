a5b52526e4e79cdd3a0318a8d98a2963d64abc5e
Johan Arturo Alvarado Hernández <johan@tangosource.com>
2013-08-15 11:47:41 -0500

    rescue files

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,28 +1,34 @@
 class Login < ActiveRecord::Base
+
   attr_accessor :reemail
   attr_accessor :repassword
-
   #only presence all
   def self.presence?(instance,params)    
+    @array_params = []
     @instance = instance
     @params = params
     @params.each{|key, value|
       if value.length == 0
         #puts "key = #{key} and v= #{value}"
         @instance.errors.add(key.parameterize.underscore.to_sym,'Please enter all methods')           
+      else
+        @array_params << key
       end
     }
     compareTo?
    # binding.pry
   end
   def self.compareTo?
-    if @params['email'] != @params['reemail']
-      @instance.errors.add('email','Email not match with reemail')           
+    if @array_params.include?('email') and @array_params.include?('reemail') and @params['email'] != @params['reemail']
+      @instance.errors.add('email','Email not match with reemail')
+    else
+      verify_email(@params['email'])
     end
-    if @params['password'] != @params['repassword']
+    if @array_params.include?('password') and @array_params.include?('repassword') and @params['password'] != @params['repassword']
       @instance.errors.add('password','Password not match with reepasswors')
-    end
+    else
+      verify_password(@params['password'])
+    end  
     @instance
-
   end
 end
