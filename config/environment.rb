424b9241fa9ef307ab4064b5e568db3562cc6d6d
Johan Arturo Alvarado Hernández <johan@tangosource.com>
2013-08-19 11:39:30 -0500

    change irb to pry in rails console

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Kickstarter::Application.initialize!
+silence_warnings do
+        begin
+            require 'pry'
+            IRB = Pry
+        rescue LoadError
+        end
+    end
+
