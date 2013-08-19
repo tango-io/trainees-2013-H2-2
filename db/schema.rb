6a1f78a2cc8baa2bc905e57434b3ab435d6ed200
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-08-19 10:49:19 -0500

    mend

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130815035941) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "backers", force: true do |t|
     t.string   "user_id"
     t.string   "project_id"
