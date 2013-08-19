92619cf31fa2e920114a6264b13cc310f12b9125
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-08-19 18:46:41 -0500

    Fix categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,27 @@
 class CategoriesController < ApplicationController
   def index
   end
+  
   def show
     @category = Category.find(params[:id]);
-    @projects_category = @category.projects.first(3)
+    @name_section = @category.name
+    @projects_section = @category.projects.first(3)
+    @categories = Category.order("created_at")
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(params_category)
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+  def params_category
+    params.require(:category).permit(:name)
   end
 end
