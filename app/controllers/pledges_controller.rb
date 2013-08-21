152852f12fe47baec1d3b9d3fd09437ac7b72b94
Jesus Romero <jose.romero@tangosource.com>
2013-08-20 19:02:18 -0500

    Added date with format dd/Month/yyyy

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -22,6 +22,6 @@ before_action :set_project
  end
  
  def pledges_params
-  params.require(:pledge).permit(:name, :description, :project_id, :amount) 
+  params.require(:pledge).permit(:name, :description, :project_id, :amount, :date) 
  end
 end
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,5 @@
 class Pledge < ActiveRecord::Base
   validates :name, :description, :amount, presence: true
   validates :amount, numericality: true
+  validates_format_of :date, :with => /^(\d\d)\/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\/201(3|4)/, :multiline => true, :messae => "date format error Example 20/Aug/2014"
 end
