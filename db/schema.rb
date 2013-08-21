886267b9055980c161d1e95cd1bed507c2ebd53b
Antonio Chavez <cavjzz@gmail.com>
2013-08-21 06:55:37 -0500

    devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,11 +1,24 @@
-ActiveRecord::Schema.define(version: 20130812123318) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130821104429) do
 
   create_table "backers", force: true do |t|
-    t.string   "user_id"
-    t.string   "project_id"
     t.float    "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "project_id"
   end
 
   create_table "categories", force: true do |t|
@@ -22,11 +35,11 @@ ActiveRecord::Schema.define(version: 20130812123318) do
   end
 
   create_table "comments", force: true do |t|
-    t.string   "user_id"
     t.string   "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
+    t.integer  "user_id"
   end
 
   create_table "pages", force: true do |t|
@@ -38,17 +51,16 @@ ActiveRecord::Schema.define(version: 20130812123318) do
   end
 
   create_table "pledges", force: true do |t|
-    t.string   "project_id"
     t.string   "name"
     t.string   "description"
     t.float    "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
   create_table "projects", force: true do |t|
     t.string   "name"
-    t.string   "user_id"
     t.string   "description"
     t.text     "content"
     t.string   "video"
@@ -65,6 +77,7 @@ ActiveRecord::Schema.define(version: 20130812123318) do
     t.datetime "updated_at"
     t.string   "picture"
     t.string   "location"
+    t.integer  "user_id"
   end
 
   create_table "subcategories", force: true do |t|
