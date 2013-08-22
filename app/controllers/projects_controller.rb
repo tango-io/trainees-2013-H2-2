c39fba1f8c8a8b23823bcf8f530f2236ae490940
Jesus Romero <jose.romero@tangosource.com>
2013-08-22 18:13:57 -0500

    add dynamic period to show projects view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,9 +11,8 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    #date = @project.crated_at.split("-")
-    #@year = date[0]
-    #@month = date[1] 
+    @dead_line = @project.created_at + @project.period.days
+    @count = (@dead_line.to_date - Time.now.to_date).to_i 
     @pledges = Pledge.where(:project_id => @project.id)
   end
 
