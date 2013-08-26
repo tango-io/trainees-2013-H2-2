1b47ead4c3512361006070a97dde986c4b01ebee
Alejandra Stephanía Cernas Larios <alejandra.cernas@tangosource.com>
2013-08-26 15:49:53 -0500

    created relational filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   
   mount_uploader :avatar, PictureUploader   
   def set_default_values
      self.avatar = "no-img.png" unless self.avatar
   end
+
+
+  has_many :approved_comments , -> { where approved: true }, class_name: 'Comment'
 
 end
