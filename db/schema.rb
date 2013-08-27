09136d7c95d92ddedaa82370fdd9a8e621f18c8f
Jesus Romero <jose.romero@tangosource.com>
2013-08-27 10:19:05 -0500

    test features

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,17 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20130827143240) do
-  enable_extension "plpgsql"
 
   create_table "backers", force: true do |t|
     t.float    "amount"
@@ -28,6 +40,14 @@ ActiveRecord::Schema.define(version: 20130827143240) do
     t.datetime "updated_at"
     t.integer  "project_id"
     t.integer  "user_id"
+  end
+
+  create_table "faqs", force: true do |t|
+    t.integer  "project_id"
+    t.string   "question"
+    t.text     "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "pages", force: true do |t|
