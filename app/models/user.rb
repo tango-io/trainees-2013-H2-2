e86c4c81f6b251217e20d03b94f33eefc2908737
Alejandra Stephanía Cernas Larios <alejandra.cernas@tangosource.com>
2013-08-27 11:37:47 -0500

    Removed :approved_comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,12 @@
 class User < ActiveRecord::Base
   before_save :set_default_values
-
   has_many :projects, :dependent => :destroy
-
+  has_many :comments 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   
   mount_uploader :avatar, PictureUploader   
   def set_default_values
      self.avatar = "no-img.png" unless self.avatar
   end
-
-
-  has_many :approved_comments , -> { where approved: true }, class_name: 'Comment'
-
 end
