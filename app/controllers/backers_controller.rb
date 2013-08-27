55ff2c2aee3cd140261dd9d8ccbb56e6861fc545
Jesus Romero <jose.romero@tangosource.com>
2013-08-27 12:45:17 -0500

    create new backer method and view with basic style

--- app/controllers/backers_controller.rb
+++ app/controllers/backers_controller.rb
@@ -1,3 +1,18 @@
 class BackersController < ApplicationController
+ before_action :set_project_id
 
+ def index
+     
+ end
+
+ def new
+  @backer = @project.backers.new
+  @pledges = @project.pledges.where(:project_id => @project.id)
+ end
+
+ private
+ def set_project_id
+   @project = Project.find(params[:project_id])
+ end
+ 
 end
