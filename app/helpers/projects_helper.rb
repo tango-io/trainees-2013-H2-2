837ae03da69f913030cf03dc38017a31fd0847cb
Jesus Romero <jose.romero@tangosource.com>
2013-08-28 10:59:58 -0500

    deleted method project_status from projects_helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,8 +1,10 @@
 module ProjectsHelper
+
   def project_visit (id)
     project = Project.find(id)
     counter = project.hit_counter + 1
     project.update(:hit_counter => counter)
   end
+
 end
 
