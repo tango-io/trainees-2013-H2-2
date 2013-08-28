186fbaf14a2afbfd99f3b05fa65bff6918407bd2
Jesus Romero <jose.romero@tangosource.com>
2013-08-28 11:47:17 -0500

    added method create and backers_params in backers controllers

--- app/controllers/backers_controller.rb
+++ app/controllers/backers_controller.rb
@@ -9,9 +9,20 @@ class BackersController < ApplicationController
   @backer = @project.backers.new
  end
 
+ def create 
+   @backer = @project.backers.new(backers_params)
+   if @backer.save
+     redirect_to @project
+   end
+ end
+
  private
  def set_project_id
    @project = Project.find(params[:project_id])
+ end
+
+ def backers_params
+   params.require(:backer).permit(:amount, :pledge_id, :project_id, :user_id)
  end
  
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,10 +1,8 @@
 module ProjectsHelper
-
-  def project_visit (id)
+ def project_visit (id)
     project = Project.find(id)
     counter = project.hit_counter + 1
     project.update(:hit_counter => counter)
   end
-
 end
 
