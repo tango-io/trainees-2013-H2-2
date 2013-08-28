333e1bb53a30beb00b4f1368a862bc3814ab9d74
Jesus Romero <jose.romero@tangosource.com>
2013-08-28 12:00:34 -0500

    added project_id and user_id to validations

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -2,6 +2,6 @@ class Backer < ActiveRecord::Base
   has_one :pledge
   belongs_to :project
 
-  validates :amount, :pledge_id, presence: true
+  validates :amount, :pledge_id, :project_id, :user_id, presence: true
   validates :amount, numericality: true
 end
