0c72285e4298bb172ad75d51cbc3eb4457ec9fd8
Alejandra Stephanía Cernas Larios <alejandra.cernas@tangosource.com>
2013-08-29 14:45:40 -0500

    created scope for non approved comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ActiveRecord::Base
-
   mount_uploader :picture, PictureUploader   
   belongs_to :user
   belongs_to :category
@@ -9,6 +8,7 @@ class Project < ActiveRecord::Base
   has_many :pledges
   has_many :backers
   has_many :faqs
+
   validates :name, :description, :content, :goal, :video, :period, presence: true
   validates :goal, numericality: true
   validates_format_of :video, :with => /^(\/\/)?(www\.)?youtube.com\/embed\/([a-z0-9-]+)/i, :multiline => true, :message => "Invalid format"                           
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   mount_uploader :avatar, PictureUploader   
 
 
