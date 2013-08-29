31a577c77ff27e97018eaea4c342cf9fc3d8852f
Jesus Romero <jose.romero@tangosource.com>
2013-08-29 14:53:21 -0500

    refactored method noreward and validate in backersController

--- app/assets/javascripts/angular/controllers/backersController.js
+++ app/assets/javascripts/angular/controllers/backersController.js
@@ -7,16 +7,30 @@ $scope.validate = function(id){
   amount = $("input[name='backer[amount]']");
   amount = parseInt(amount.val());
   if (pledge > amount || isNaN(amount)) {
-
-    $("#aviable_message").html("pledge is no aviable").css("background-color", "#DF0101");
     $("input[name='backer[pledge_id]']").val([]); 
+    if (isNaN(amount)) {
+      $("#aviable_message").html("You must put the amount").css("background-color", "#DF0101");
+    } else {
+        $("#aviable_message").html("pledge is no aviable").css("background-color", "#DF0101");
+      }
   }
   else {
     $("#aviable_message").html("pledge is aviable").css("background-color", "#04B404");
    }
  };
 
-  
+ $scope.noreward = function(){
+   console.log("vacio");
+   amount = $("input[name='backer[amount]']");
+   amount = parseInt(amount.val());
+   if (isNaN(amount)) {
+     $("#aviable_message").html("You must put the amount").css("background-color", "#DF0101");
+     $("input[name='backer[pledge_id]']").val([]); 
+   } else {
+     $("#aviable_message").html("You choosed no reward").css("background-color", "#04B404");
+   }
+
+ }; 
 
  
 
