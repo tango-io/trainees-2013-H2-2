4576a9be5248aaa0edd899eb6e5944f0ba474a90
Hector Alejandro Meza Valdez <hmezav@gmail.com>
2013-09-02 18:29:10 -0500

    message index started and comments model fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,8 @@ class Comment < ActiveRecord::Base
     joins(:project).where(projects: {user_id: current_user_id})
   end
 
+  scope :approved, ->(project_id) do
+    joins(:user).where(:project_id => project_id, :approved => :true).order("created_at DESC")
+  end
+
 end
