6b83a033a1ac27eda531e6cda50b323dcd742463
Jesus Romero <jose.romero@tangosource.com>
2013-09-02 12:35:53 -0500

    modified format date in new pledges

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,8 +1,10 @@
 class Pledge < ActiveRecord::Base
 
   has_many :backers
-  validates :name, :description, :amount, presence: true
+  validates :name, :description, :amount, :date, presence: true
   validates :amount, numericality: true
 
-  validates_format_of :date, :with => /^(\d\d)\/(\d\d)\/201(3|4)/, :multiline => true, :messae => "date format error Example 20/Aug/2014"
+  #validates_format_of :date, :with => /^(\d\d)-(\d\d)-201(3|4)/, :multiline => true, :messae => "date format error Example 20/Aug/2014"
+  scope :actives, -> {where((:date.to_date - Time.now.to_date) > 0 )}
+
 end
