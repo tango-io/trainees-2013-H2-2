3c3c6fe631bb68248f9766294c19eeba9ecd2685
Jesus Romero <jose.romero@tangosource.com>
2013-09-02 15:30:00 -0500

    modified update project, video field

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,7 +10,7 @@ class Project < ActiveRecord::Base
   has_many :backers
   has_many :faqs
   validates :name, :description, :content, :goal, :video, :period, presence: true
-  validates :goal, :period, numericality: true
+  validates :goal, numericality: true
   validates_format_of :video, :with => /^(\/\/)?(www\.)?youtube.com\/embed\/([a-z0-9-]+)/i, :multiline => true, :message => "Invalid format"                           
 end
 
