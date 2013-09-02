d656238f863c194e4afafef88819a91d43569fdc
Jesus Romero <jose.romero@tangosource.com>
2013-09-02 12:50:13 -0500

    added scope validates in Pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -5,6 +5,6 @@ class Pledge < ActiveRecord::Base
   validates :amount, numericality: true
 
   #validates_format_of :date, :with => /^(\d\d)-(\d\d)-201(3|4)/, :multiline => true, :messae => "date format error Example 20/Aug/2014"
-  scope :actives, -> {where((:date.to_date - Time.now.to_date) > 0 )}
+  scope :actives, -> {where("date > :actual_date", actual_date: Time.now)}
 
 end
